bdbdd2deb09a3ba17f18c2724e0f3963062510dc
DaisukiTamago <gustavoalvez820@gmail.com>
2024-08-19 18:53:25 -0300

    chore: :bug: eslint ignore build folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {ignores: ["dist"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
