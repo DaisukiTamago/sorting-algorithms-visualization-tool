50c02c4dac1f7c9e19aadd3ea256a0da7b972659
DaisukiTamago <gustavoalvez820@gmail.com>
2024-08-27 16:43:36 -0300

    feat: :sparkles: add settings modal

--- src/core/domHandler.ts
+++ src/core/domHandler.ts
@@ -1,16 +1,22 @@
 const canvasElement = document.getElementById("renderer");
+const settingsButton = document.getElementById("settings");
+const settingsModal = document.querySelector("dialog");
 const shuffleButton = document.getElementById("shuffle") as HTMLButtonElement;
 const logButton = document.getElementById("log") as HTMLButtonElement;
 const sortButton = document.getElementById("sort") as HTMLButtonElement;
 const selectElement = document.getElementById("select") as HTMLSelectElement;
 const controlsContainer = document.getElementById("controls") as HTMLDivElement;
 
+settingsButton.onclick = toggleSettings;
+
+function toggleSettings() {
+  settingsModal.showModal();
+}
+
 function enableControls() {
-  if (selectElement.value != "12345") {
-    sortButton.disabled = false;
-  }
   shuffleButton.disabled = false;
   selectElement.disabled = false;
+  sortButton.disabled = false;
 }
 
 function disableControls() {
